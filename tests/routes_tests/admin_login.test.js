4f3f702b1b5b6f145a10fd183d5c97b921f8cea1
RogeredBacon <jinglis12@googlemail.com>
2018-02-26 22:44:37 +0000

    visitor_check tests are working however still working on admin_login tests. Getting similar but different 500 errors to the visitor_name tests #218

--- tests/routes_tests/admin_login.test.js
+++ tests/routes_tests/admin_login.test.js
@@ -1,35 +1,40 @@
 const test = require('tape');
 const request = require('supertest');
 const app = require('../../react-backend/app');
+const jwt = require('jsonwebtoken');
 
 test('POST /api/admin/login | password match database hash', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const successPayload = {
     password: 'James',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(successPayload)
     .expect(200)
     .expect('Content-Type', /json/)
-    .expect(res => res.json())
     .end((err, res) => {
-      t.ok(res.success, 'Password correctly matched database hash');
+      t.equal(res.body.success, true);
       t.end();
     });
 });
 
 test('POST /api/admin/login | no password match for database hash', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const failurePayload = {
     password: 'Zenith',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(failurePayload)
     .expect(200)
     .expect('Content-Type', /json/)
-    .expect(res => res.json())
     .end((err, res) => {
-      t.ok(!res.success, 'Password did not match the database hash');
+      t.notEqual(res.body.success, false);
       t.end();
     });
 });
--- tests/routes_tests/visitor_check.test.js
+++ tests/routes_tests/visitor_check.test.js
@@ -1,105 +1,118 @@
 const test = require('tape');
 const request = require('supertest');
 const app = require('../../react-backend/app');
+const jwt = require('jsonwebtoken');
 
 test('POST /api/visit/check | viable name & email & exists in database', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const successPayload = {
     formSender: 'James Bond',
     formEmail: 'hello@yahoo.com',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(successPayload)
     .expect(200)
     .expect('Content-Type', /text/)
-    .expect(res => res.text())
     .end((err, res) => {
-      t.ok(res === 'true', 'Name and Email are viable and found in database');
+      t.equal(res.text, 'true');
       t.end();
     });
 });
 
 test('POST /api/visit/check | viable name & email & not found in database', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const successPayload = {
     formSender: 'John The Ripper',
     formEmail: 'goodbye@yahoo.com',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(successPayload)
     .expect(200)
     .expect('Content-Type', /text/)
-    .expect(res => res.text())
     .end((err, res) => {
-      t.ok(res === 'false', 'Name and Email are viable and not found in database');
+      t.equal(res.text, 'false');
       t.end();
     });
 });
 
 test('POST /api/visit/check | bad name & viable email', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const failPayload = {
     formSender: 'addi7837***&&$$%$%',
     formEmail: 'hello@yahoo.com',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(failPayload)
     .expect(415)
     .expect('Content-Type', /text/)
-    .expect(res => res.text())
     .end((err, res) => {
-      t.ok(res === 'name', 'Name is not viable, Email is viable');
+      t.equal(res.text, 'name');
       t.end();
     });
 });
 
 test('POST /api/visit/check | viable name & bad email', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const failPayload = {
     formSender: 'James Bond',
     formEmail: 'helloyahoo.com',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(failPayload)
     .expect(415)
     .expect('Content-Type', /text/)
-    .expect(res => res.text())
     .end((err, res) => {
-      t.ok(res === 'email', 'Name is viable, email is not viable');
+      t.equal(res.text, 'email');
       t.end();
     });
 });
 
 test('POST /api/visit/check | bad name & email', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const failPayload = {
     formSender: 'Jame77***"%s Bond',
     formEmail: 'helloyahoo.com',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(failPayload)
     .expect(415)
     .expect('Content-Type', /text/)
-    .expect(res => res.text())
     .end((err, res) => {
-      t.ok(res === 'emailname', 'Name and email are not viable');
+      t.equal(res.text, 'emailname');
       t.end();
     });
 });
 
 test('POST /api/visit/check | no input test', (t) => {
+  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
+
   const failPayload = {
     formSender: '',
     formEmail: '',
   };
   request(app)
     .post('/api/visit/check')
+    .set('authorization', token)
     .send(failPayload)
     .expect(415)
     .expect('Content-Type', /text/)
-    .expect(res => res.text())
     .end((err, res) => {
-      t.ok(res === 'noinput', 'No input');
+      t.equal(res.text, 'noinput');
       t.end();
     });
 });
